c7977a18e285df82898b92825055783f5719dff5
Bence Frenyó <bence.frenyo@nordicsemi.no>
2018-11-08 11:04:46 +0100

    Change file-loader to url-loader for resources, bump version

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -83,7 +83,7 @@ module.exports = {
             ],
         }, {
             test: /\.(png|gif|ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-            loader: require.resolve('file-loader'),
+            loader: require.resolve('url-loader'),
         }],
     },
     resolve: {
