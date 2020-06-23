b889f2a8b33c96629d234ee57429812407b16366
Marko Schulz <Marko.Schulz@nordicsemi.no>
2020-06-23 16:10:05 +0200

    Correct comment

--- mocks/electronMock.js
+++ mocks/electronMock.js
@@ -1,8 +1,4 @@
-// Allows jest to test files that import less or css. Jest cannot
-// parse things like this so we have to mock them. This mock just returns
-// an empty object for files that match the moduleNameMapper expression
-// in package.json.
-// Ref: https://facebook.github.io/jest/docs/tutorial-webpack.html
+// Allows jest to test files that import the electron package.
 
 module.exports = {
     require: jest.fn(),
