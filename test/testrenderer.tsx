60374ef9f2e7810705b827fae06ad93ab45aea98
Marko Schulz <Marko.Schulz@nordicsemi.no>
2021-12-02 11:44:10 +0100

    Convert testrenderer to TypeScript

--- test/testrenderer.tsx
+++ test/testrenderer.tsx
@@ -7,19 +7,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
-import { createStore } from 'redux';
+import { Action, createStore } from 'redux';
 
 import rootReducer from '../src/rootReducer';
 
-const createPreparedStore = actions => {
+const createPreparedStore = (actions: Action[]) => {
     const store = createStore(rootReducer());
     actions.forEach(store.dispatch);
 
     return store;
 };
 
-const PreparedProvider = actions => props =>
+const PreparedProvider = (actions: Action[]) => (props: unknown) =>
     <Provider store={createPreparedStore(actions)} {...props} />;
 
-export default (element, actions = []) =>
+export default (element: React.ReactElement, actions: Action[] = []) =>
     render(element, { wrapper: PreparedProvider(actions) });
