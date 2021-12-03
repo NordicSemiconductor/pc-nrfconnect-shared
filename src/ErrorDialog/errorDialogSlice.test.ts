45d7e62acebc76844d065ec6ea78edb3e70cf125
Marko Schulz <Marko.Schulz@nordicsemi.no>
2021-12-03 15:11:11 +0100

    Ease testing state after applying multiple actions
    The new function `dispatchTo` makes it easier get the state a reducer
    produces after one or multiple actions are applied to it.

--- src/ErrorDialog/errorDialogSlice.test.ts
+++ src/ErrorDialog/errorDialogSlice.test.ts
@@ -4,59 +4,53 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
+import dispatchTo from '../../test/dispatchTo';
 import { hideDialog, reducer, showDialog } from './errorDialogSlice';
-
-const initialState = reducer(undefined, { type: '' });
 
 const anErrorMessage = 'An error occurred';
 const anotherErrorMessage = 'Another error occurred';
 
 describe('errorDialogReducer', () => {
     it('should be hidden by default', () => {
+        const initialState = dispatchTo(reducer, [{ type: '@INIT' }]);
+
         expect(initialState.isVisible).toEqual(false);
     });
 
     it('should be visible and have the supplied message after show action has been dispatched', () => {
-        const state = reducer(initialState, showDialog(anErrorMessage, {}));
-        expect(state.isVisible).toEqual(true);
-        expect(state.messages).toContain(anErrorMessage);
+        const withAnError = dispatchTo(reducer, [
+            showDialog(anErrorMessage, {}),
+        ]);
+        expect(withAnError.isVisible).toEqual(true);
+        expect(withAnError.messages).toContain(anErrorMessage);
     });
 
     it('should be visible and have all messages after multiple show actions have been dispatched', () => {
-        const withAnError = reducer(
-            initialState,
-            showDialog(anErrorMessage, {})
-        );
-        const withBothErrors = reducer(
-            withAnError,
-            showDialog(anotherErrorMessage, {})
-        );
-        expect(withBothErrors.isVisible).toEqual(true);
-        expect(withBothErrors.messages).toContain(anErrorMessage);
-        expect(withBothErrors.messages).toContain(anotherErrorMessage);
+        const withTwoErrors = dispatchTo(reducer, [
+            showDialog(anErrorMessage, {}),
+            showDialog(anotherErrorMessage, {}),
+        ]);
+        expect(withTwoErrors.isVisible).toEqual(true);
+        expect(withTwoErrors.messages).toContain(anErrorMessage);
+        expect(withTwoErrors.messages).toContain(anotherErrorMessage);
     });
 
     it('should not add message if it already exists in list', () => {
-        const withAnError = reducer(
-            initialState,
-            showDialog(anErrorMessage, {})
-        );
-        const stillOnlyWithOneError = reducer(
-            withAnError,
-            showDialog(anErrorMessage, {})
-        );
+        const withAnError = dispatchTo(reducer, [
+            showDialog(anErrorMessage, {}),
+            showDialog(anErrorMessage, {}),
+        ]);
 
-        expect(stillOnlyWithOneError.messages).toEqual([anErrorMessage]);
+        expect(withAnError.messages).toEqual([anErrorMessage]);
     });
 
     it('should set dialog to invisible and clear message list when hide action has been dispatched', () => {
-        const withAnError = reducer(
-            initialState,
-            showDialog(anErrorMessage, {})
-        );
-        const cleared = reducer(withAnError, hideDialog());
+        const withAClearedError = dispatchTo(reducer, [
+            showDialog(anErrorMessage, {}),
+            hideDialog(),
+        ]);
 
-        expect(cleared.isVisible).toEqual(false);
-        expect(cleared.messages.length).toEqual(0);
+        expect(withAClearedError.isVisible).toEqual(false);
+        expect(withAClearedError.messages.length).toEqual(0);
     });
 });
