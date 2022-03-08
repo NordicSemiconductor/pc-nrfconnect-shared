bba5a1c0dfa625df84cb5061bc11b958503abee7
Marko Schulz <Marko.Schulz@nordicsemi.no>
2022-03-08 16:19:53 +0100

    Use common jest config also in shared
    For this, make the sub directory of the config configurable and also
    add the possibility to disable some mocks (which is also need for the
    CM app).

--- config/jest.config.js
+++ config/jest.config.js
@@ -4,41 +4,36 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-module.exports = () => ({
+module.exports = (
+    subDir = '/node_modules/pc-nrfconnect-shared',
+    disabledMocks = []
+) => ({
     testEnvironment: 'jsdom',
     moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$':
-            '<rootDir>/node_modules/pc-nrfconnect-shared/mocks/fileMock.ts',
-        '\\.(css|scss)$':
-            '<rootDir>/node_modules/pc-nrfconnect-shared/mocks/emptyMock.ts',
-        packageJson$:
-            '<rootDir>/node_modules/pc-nrfconnect-shared/mocks/packageJsonMock.ts',
-        'pc-ble-driver-js':
-            '<rootDir>/node_modules/pc-nrfconnect-shared/mocks/bleDriverMock.ts',
-        'pc-nrfjprog-js|nrf-device-setup|serialport|usb':
-            '<rootDir>/node_modules/pc-nrfconnect-shared/mocks/emptyMock.ts',
-        '^electron$':
-            '<rootDir>/node_modules/pc-nrfconnect-shared/mocks/electronMock.ts',
-        '^electron-store$':
-            '<rootDir>/node_modules/pc-nrfconnect-shared/mocks/electronStoreMock.ts',
-        '^react-ga$':
-            '<rootDir>/node_modules/pc-nrfconnect-shared/mocks/gaMock.ts',
-        '@nordicsemiconductor/nrf-device-lib-js':
-            '<rootDir>/node_modules/pc-nrfconnect-shared/mocks/deviceLibMock.ts',
-        '@electron/remote':
-            '<rootDir>/node_modules/pc-nrfconnect-shared/mocks/remoteMock.ts',
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': `<rootDir>${subDir}/mocks/fileMock.ts`,
+        '\\.(css|scss)$': `<rootDir>${subDir}/mocks/emptyMock.ts`,
+        packageJson$: `<rootDir>${subDir}/mocks/packageJsonMock.ts`,
+        'pc-ble-driver-js': `<rootDir>${subDir}/mocks/bleDriverMock.ts`,
+        'pc-nrfjprog-js|nrf-device-setup|usb': `<rootDir>${subDir}/mocks/emptyMock.ts`,
+        '^electron$': `<rootDir>${subDir}/mocks/electronMock.ts`,
+        '^electron-store$': `<rootDir>${subDir}/mocks/electronStoreMock.ts`,
+        '@nordicsemiconductor/nrf-device-lib-js': `<rootDir>${subDir}/mocks/deviceLibMock.ts`,
+        '@electron/remote': `<rootDir>${subDir}/mocks/remoteMock.ts`,
+        ...(disabledMocks.includes('react-ga')
+            ? {}
+            : { '^react-ga$': `<rootDir>${subDir}/mocks/gaMock.ts` }),
+        ...(disabledMocks.includes('serialport')
+            ? {}
+            : { serialport: `<rootDir>${subDir}/mocks/emptyMock.ts` }),
     },
     transform: {
         '^.+\\.[jt]sx?$': [
             'babel-jest',
             {
-                configFile:
-                    './node_modules/pc-nrfconnect-shared/config/babel.config.js',
+                configFile: `.${subDir}/config/babel.config.js`,
             },
         ],
     },
     transformIgnorePatterns: ['node_modules/(?!(pc-nrfconnect-shared)/)'],
-    setupFilesAfterEnv: [
-        '<rootDir>/node_modules/pc-nrfconnect-shared/test/setupTests.ts',
-    ],
+    setupFilesAfterEnv: [`<rootDir>${subDir}/test/setupTests.ts`],
 });
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+module.exports = require('./config/jest.config')('', ['react-ga']);
