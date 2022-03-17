39645af8132ca8dc18072a5925885bfcc99cc8f5
Marko Schulz <Marko.Schulz@nordicsemi.no>
2022-03-17 14:11:28 +0100

    Fix: Add missing mock

--- mocks/remoteMock.ts
+++ mocks/remoteMock.ts
@@ -7,6 +7,7 @@
 // Allows jest to test files that import the electron package.
 
 export const require = jest.fn();
+export const dialog = jest.fn();
 export const getCurrentWindow = () => ({
     reload: jest.fn(),
 });
