e14014d701f2d49edc835b38ffffc105ba63d4b3
Marko Schulz <Marko.Schulz@nordicsemi.no>
2022-03-18 15:06:09 +0100

    Enable disabling packageJson mock

--- config/jest.config.js
+++ config/jest.config.js
@@ -13,13 +13,17 @@ module.exports = (disabledMocks = []) => ({
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': `${mockDir}/fileMock.ts`,
         '\\.(css|scss)$': `${mockDir}/emptyMock.ts`,
-        packageJson$: `${mockDir}/packageJsonMock.ts`,
         'pc-ble-driver-js': `${mockDir}/bleDriverMock.ts`,
         'pc-nrfjprog-js|nrf-device-setup|usb': `${mockDir}/emptyMock.ts`,
         '^electron$': `${mockDir}/electronMock.ts`,
         '^electron-store$': `${mockDir}/electronStoreMock.ts`,
         '@nordicsemiconductor/nrf-device-lib-js': `${mockDir}/deviceLibMock.ts`,
         '@electron/remote': `${mockDir}/remoteMock.ts`,
+        ...(disabledMocks.includes('packageJson')
+            ? {}
+            : {
+                  packageJson$: `${mockDir}/packageJsonMock.ts`,
+              }),
         ...(disabledMocks.includes('react-ga')
             ? {}
             : { '^react-ga$': `${mockDir}/gaMock.ts` }),
