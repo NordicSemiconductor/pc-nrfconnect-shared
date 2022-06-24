0156a50a7f1f6d6241c86e1d7e0fc964c486c842
Jesper Norsted <jesper.norsted@nordicsemi.no>
2022-06-24 14:15:44 +0200

    Copy modal files and integrate into shared

---
+++ src/Shortcuts/ShortcutItem.tsx
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+import React, { FC } from 'react';
+import Card from 'react-bootstrap/Card';
+import { string } from 'prop-types';
+
+import './Shortcut-item.scss';
+
+export interface Props {
+    title: string;
+    hotKey: string;
+    description: string;
+}
+
+const ShortcutItem: FC<Props> = ({ title, hotKey, description }) => {
+    const shortcutKeys: string[] = hotKey.split('+');
+    return (
+        <Card className="shortcut-item">
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    {shortcutKeys.map(shortcutKey => (
+                        <span key={shortcutKey}>
+                            <span className="shortcut-span">{shortcutKey}</span>
+                            {shortcutKey !==
+                            shortcutKeys[shortcutKeys.length - 1] ? (
+                                <span className="separator-span">+</span>
+                            ) : null}
+                        </span>
+                    ))}
+                </Card.Text>
+                <Card.Text>{description}</Card.Text>
+            </Card.Body>
+        </Card>
+    );
+};
+
+ShortcutItem.propTypes = {
+    title: string.isRequired,
+    hotKey: string.isRequired,
+    description: string.isRequired,
+};
+
+export default ShortcutItem;
---
+++ src/Shortcuts/ShortcutModal.tsx
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2015 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+import React, { FC } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { arrayOf, bool, func, shape, string } from 'prop-types';
+
+import ShortcutItem from './ShortcutItem';
+import { Shortcut } from './useHotkey';
+
+import './Shortcut-modal.scss';
+
+export interface Props {
+    isVisible: boolean;
+    onCancel: () => void;
+    localShortcutList: Shortcut[];
+    globalShortcutList: Shortcut[];
+}
+
+const ShortcutModal: FC<Props> = ({
+    isVisible,
+    onCancel,
+    localShortcutList,
+    globalShortcutList,
+}) => (
+    <Modal show={isVisible} onHide={onCancel} className="shortcut-modal">
+        <Modal.Header closeButton>
+            <Modal.Title>
+                <h3>Shortcuts</h3>
+            </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="shortcut-lists">
+            <div>
+                <h4>In this app</h4>
+                {localShortcutList.map(shortcut => (
+                    <ShortcutItem
+                        key={shortcut.hotKey}
+                        title={shortcut.title}
+                        hotKey={shortcut.hotKey}
+                        description={shortcut.description}
+                    />
+                ))}
+            </div>
+            <div>
+                <h4>In all apps</h4>
+                {globalShortcutList.map(shortcut => (
+                    <ShortcutItem
+                        key={shortcut.hotKey}
+                        title={shortcut.title}
+                        hotKey={shortcut.hotKey}
+                        description={shortcut.description}
+                    />
+                ))}
+            </div>
+        </Modal.Body>
+    </Modal>
+);
+
+ShortcutModal.propTypes = {
+    isVisible: bool.isRequired,
+    onCancel: func.isRequired,
+    localShortcutList: arrayOf(
+        shape({
+            hotKey: string.isRequired,
+            title: string.isRequired,
+            description: string.isRequired,
+            action: func.isRequired,
+        }).isRequired
+    ).isRequired,
+    globalShortcutList: arrayOf(
+        shape({
+            hotKey: string.isRequired,
+            title: string.isRequired,
+            description: string.isRequired,
+            action: func.isRequired,
+        }).isRequired
+    ).isRequired,
+};
+
+export default ShortcutModal;
