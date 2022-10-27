be72a015127ca8f4278640ac4225b6b86b8ed87c
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2022-10-27 08:22:51 +0200

    Fix Item does not not need to be thes same object but same value

--- src/Dropdown/Dropdown.tsx
+++ src/Dropdown/Dropdown.tsx
@@ -89,7 +89,9 @@ const Dropdown: React.FC<DropdownProps> = ({
                 disabled={disabled}
             >
                 <span>
-                    {items.indexOf(selected) === -1 ? '' : selected.label}
+                    {items.findIndex(e => e.value === selected.value) === -1
+                        ? ''
+                        : selected.label}
                 </span>
                 <img className={styles.image} src={chevron} alt="" />
             </button>
