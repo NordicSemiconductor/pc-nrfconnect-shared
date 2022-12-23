14510c36801e411044a4e0b16fc85c204a072f18
kylebonniciIME <32518675+kylebonniciIME@users.noreply.github.com>
2022-12-23 14:39:51 +0100

    Add persistTerminal Settings

--- src/index.ts
+++ src/index.ts
@@ -70,6 +70,8 @@ export {
     getAppSpecificStore as getPersistentStore,
     persistSerialPort,
     getPersistedSerialPort,
+    persistTerminalSettings,
+    getTerminalSettings,
 } from './utils/persistentStore';
 
 export { selectedDevice } from './Device/deviceSlice';
