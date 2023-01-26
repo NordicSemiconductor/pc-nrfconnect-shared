f3843c5428608f3a129356244e3ecfc8d358eece
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-01-26 09:02:37 +0100

    Fix: Some needed types were not exported

--- Changelog.md
+++ Changelog.md
@@ -6,6 +6,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to
 [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+### Fixed
+
+-   Some types needed by the launcher were not exported.
+
 ## 6.17.3 - 2023-01-24
 
 ### Fixed
--- src/index.ts
+++ src/index.ts
@@ -92,4 +92,9 @@ export type { Props as DeviceSelectorProps } from './Device/DeviceSelector/Devic
 export type { PaneProps } from './App/App';
 export type { DfuImage } from './Device/initPacket';
 export type { DeviceSetup } from './Device/deviceSetup';
-export type { PackageJson } from './utils/AppTypes';
+export type {
+    AppInfo,
+    AppVersions,
+    PackageJson,
+    SourceJson,
+} from './utils/AppTypes';
