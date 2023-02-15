b860e2a492120f61bf877339cfc4ddbd08055d90
kylebonnici <kylebonnici@hotmail.com>
2023-02-15 19:44:45 +0100

    Update Types

--- typings/generated/src/SerialPort/SerialPort.d.ts
+++ typings/generated/src/SerialPort/SerialPort.d.ts
@@ -1,16 +1,9 @@
 /// <reference types="node" />
-import type {
-    AutoDetectTypes,
-    SetOptions,
-    UpdateOptions,
-} from '@serialport/bindings-cpp';
+import type { AutoDetectTypes, SetOptions, UpdateOptions } from '@serialport/bindings-cpp';
 import type { SerialPortOpenOptions } from 'serialport';
 import { OverwriteOptions } from '../../main';
 export declare type SerialPort = Awaited<ReturnType<typeof createSerialPort>>;
-export declare const createSerialPort: (
-    options: SerialPortOpenOptions<AutoDetectTypes>,
-    overwriteOptions?: OverwriteOptions
-) => Promise<{
+export declare const createSerialPort: (options: SerialPortOpenOptions<AutoDetectTypes>, overwriteOptions?: OverwriteOptions) => Promise<{
     path: string;
     write: (data: string | number[] | Buffer) => void;
     close: () => Promise<void>;
@@ -21,8 +14,6 @@ export declare const createSerialPort: (
     onClosed: (handler: () => void) => () => void;
     onUpdate: (handler: (newOptions: UpdateOptions) => void) => () => void;
     onSet: (handler: (newOptions: SetOptions) => void) => () => void;
-    onChange: (
-        handler: (newOptions: SerialPortOpenOptions<AutoDetectTypes>) => void
-    ) => () => void;
+    onChange: (handler: (newOptions: SerialPortOpenOptions<AutoDetectTypes>) => void) => () => void;
     onDataWritten: (handler: (data: Uint8Array) => void) => () => void;
 }>;
--- typings/generated/src/index.d.ts
+++ typings/generated/src/index.d.ts
@@ -1,30 +1,15 @@
 declare const ErrorDialogActions: {
-    hideDialog: import('@reduxjs/toolkit').ActionCreatorWithoutPayload<string>;
-    showDialog: import('@reduxjs/toolkit').ActionCreatorWithPreparedPayload<
-        [
-            message: string,
-            errorResolutions?: import('./state').ErrorResolutions | undefined
-        ],
-        {
-            message: string;
-            errorResolutions: import('./state').ErrorResolutions | undefined;
-        },
-        string,
-        never,
-        never
-    >;
+    hideDialog: import("@reduxjs/toolkit").ActionCreatorWithoutPayload<string>;
+    showDialog: import("@reduxjs/toolkit").ActionCreatorWithPreparedPayload<[message: string, errorResolutions?: import("./state").ErrorResolutions | undefined], {
+        message: string;
+        errorResolutions: import("./state").ErrorResolutions | undefined;
+    }, string, never, never>;
 };
 export { ErrorDialogActions };
 export { default as App } from './App/App';
 export { default as Logo } from './Logo/Logo';
 export { default as DeviceSelector } from './Device/DeviceSelector/DeviceSelector';
-export {
-    Dialog,
-    ConfirmationDialog,
-    InfoDialog,
-    ErrorDialog,
-    DialogButton,
-} from './Dialog/Dialog';
+export { Dialog, ConfirmationDialog, InfoDialog, ErrorDialog, DialogButton, } from './Dialog/Dialog';
 export { default as Spinner } from './Dialog/Spinner';
 export { default as Slider } from './Slider/Slider';
 export { Toggle } from './Toggle/Toggle';
@@ -46,14 +31,7 @@ export { reducer as errorDialogReducer } from './ErrorDialog/errorDialogSlice';
 export { default as logger } from './logging';
 export { default as bleChannels } from './utils/bleChannels';
 export { colors } from './utils/colors';
-export {
-    setAppDirs,
-    getAppDir,
-    getAppFile,
-    getAppDataDir,
-    getAppLogDir,
-    getUserDataDir,
-} from './utils/appDirs';
+export { setAppDirs, getAppDir, getAppFile, getAppDataDir, getAppLogDir, getUserDataDir, } from './utils/appDirs';
 export { openUrl } from './utils/open';
 export { default as systemReport } from './utils/systemReport';
 export { default as usageData } from './utils/usageData';
@@ -63,13 +41,7 @@ export { default as useHotKey } from './utils/useHotKey';
 export { isDevelopment } from './utils/environment';
 export { currentPane, setCurrentPane } from './App/appLayout';
 export { isLoggingVerbose } from './Log/logSlice';
-export {
-    getAppSpecificStore as getPersistentStore,
-    persistSerialPort,
-    getPersistedSerialPort,
-    persistTerminalSettings,
-    getPersistedTerminalSettings,
-} from './utils/persistentStore';
+export { getAppSpecificStore as getPersistentStore, persistSerialPort, getPersistedSerialPort, persistTerminalSettings, getPersistedTerminalSettings, } from './utils/persistentStore';
 export { selectedDevice, getReadbackProtection } from './Device/deviceSlice';
 export { deviceInfo } from './Device/deviceInfo/deviceInfo';
 export { waitForDevice } from './Device/deviceLister';
@@ -85,9 +57,4 @@ export type { Props as DeviceSelectorProps } from './Device/DeviceSelector/Devic
 export type { PaneProps } from './App/App';
 export type { DfuImage } from './Device/initPacket';
 export type { DeviceSetup } from './Device/deviceSetup';
-export type {
-    AppInfo,
-    AppVersions,
-    PackageJson,
-    SourceJson,
-} from './utils/AppTypes';
+export type { AppInfo, AppVersions, PackageJson, SourceJson, } from './utils/AppTypes';
