1ffaa0833303b884bd490ba86c3680d364a81728
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-02-23 11:05:53 +0100

    Add Fillers

--- src/MasonryLayout/MasonryLayout.tsx
+++ src/MasonryLayout/MasonryLayout.tsx
@@ -5,6 +5,8 @@
  */
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 
+import classNames from '../utils/classNames';
+
 import styles from './masonryLayout.module.scss';
 
 interface MasonryLayoutProperties {
@@ -18,55 +20,63 @@ const MasonryLayout: React.FC<MasonryLayoutProperties> = ({
     const masonryLayoutRef = useRef<HTMLDivElement>(null);
 
     const [width, setWidth] = useState(-1);
-    const [height, setHeight] = useState(-1);
+    const [maxHeight, setMaxHeight] = useState(-1);
     const [columns, setColumns] = useState(-1);
     const [orders, setOrders] = useState<number[]>([]);
+    const [columnHeights, setColumnHeights] = useState<number[]>([]);
 
-    const generateHeightMatrix = (col: number) => {
+    const generateHeightMatrix = useCallback((col: number) => {
         let child = masonryLayoutRef.current?.firstElementChild;
         const heightMatrix: number[][] = [];
         let i = 0;
         while (child) {
             if (child instanceof HTMLElement) {
-                const rowIndex = Math.floor(i / col);
+                if (!child.className.includes('filler')) {
+                    const rowIndex = Math.floor(i / col);
 
-                if (heightMatrix[rowIndex] === undefined) {
-                    heightMatrix[rowIndex] = [];
+                    if (heightMatrix[rowIndex] === undefined) {
+                        heightMatrix[rowIndex] = [];
+                    }
+                    const row = heightMatrix[rowIndex];
+
+                    const columnIndex = i % col;
+                    row[columnIndex] = child.offsetHeight;
+
+                    i += 1;
+                } else {
+                    console.log('filler');
                 }
-                const row = heightMatrix[rowIndex];
-
-                const columnIndex = i % col;
-                row[columnIndex] = child.offsetHeight;
 
                 child = child.nextElementSibling;
-                i += 1;
             }
         }
 
         return heightMatrix;
-    };
-
-    const calcMaxHeight = useCallback((col: number): number => {
-        if (!masonryLayoutRef.current) return 0;
-
-        const heightMatrix = generateHeightMatrix(col);
-
-        const heights: number[] = Array(col).fill(0);
-        const newOrder: number[] = [];
-
-        heightMatrix.forEach(row => {
-            row.forEach(h => {
-                const smallest =
-                    heights.findIndex(v => v === Math.min(...heights)) ?? 0;
-                heights[smallest] += h;
-                newOrder.push(smallest + 1);
-            });
-        });
-
-        setOrders(newOrder);
-
-        return Math.max(...heights) + 1;
     }, []);
+
+    const calcMaxHeight = useCallback(
+        (col: number): number => {
+            if (!masonryLayoutRef.current) return 0;
+
+            const heightMatrix = generateHeightMatrix(col);
+            const heights: number[] = Array(col).fill(0);
+            const newOrder: number[] = [];
+
+            heightMatrix.forEach(row => {
+                row.forEach(itemHeight => {
+                    const smallest =
+                        heights.findIndex(h => h === Math.min(...heights)) ?? 0;
+                    heights[smallest] += itemHeight;
+                    newOrder.push(smallest + 1);
+                });
+            });
+
+            setOrders(newOrder);
+            setColumnHeights(heights);
+            return Math.max(...heights);
+        },
+        [generateHeightMatrix]
+    );
 
     useEffect(() => {
         if (masonryLayoutRef.current === null) return;
@@ -80,14 +90,12 @@ const MasonryLayout: React.FC<MasonryLayoutProperties> = ({
                     current.clientWidth /
                         (minWidth + Number.parseInt(styles.margin, 10))
                 );
-
                 if (noOfColumns !== columns) {
                     setColumns(noOfColumns);
+                    setMaxHeight(calcMaxHeight(noOfColumns));
                 }
-
-                setHeight(calcMaxHeight(noOfColumns));
             } else {
-                setHeight(calcMaxHeight(columns));
+                setMaxHeight(calcMaxHeight(columns));
             }
         });
         observer.observe(masonryLayoutRef.current);
@@ -97,23 +105,50 @@ const MasonryLayout: React.FC<MasonryLayoutProperties> = ({
         };
     }, [calcMaxHeight, columns, minWidth, width]);
 
+    useEffect(() => {
+        console.log(
+            'columnHeights',
+            columnHeights,
+            columnHeights.map((h, i) =>
+                h !== maxHeight
+                    ? {
+                          height: `${maxHeight - h}px`,
+                          order: `${i + 1}`,
+                      }
+                    : null
+            )
+        );
+    }, [columnHeights, maxHeight]);
+
     return (
         <div
             ref={masonryLayoutRef}
-            className={styles.masonryLayout}
-            style={{ maxHeight: height }}
+            className={classNames(styles.masonryLayout)}
+            style={{ maxHeight: `${maxHeight}px` }}
         >
             {React.Children.map(children, (child, i) => (
                 <div
                     style={{
                         minWidth,
-                        order: `${orders[i]}`,
+                        order: `${orders[i] ?? 1}`,
                         pageBreakBefore: `${i < columns ? 'always' : 'auto'}`,
                     }}
                 >
                     {child}
                 </div>
             ))}
+            {columnHeights.map((h, i) =>
+                h !== maxHeight ? (
+                    <div
+                        className="filler"
+                        key={`filler_${h + i}`}
+                        style={{
+                            height: `${maxHeight - h}px`,
+                            order: `${i + 1}`,
+                        }}
+                    />
+                ) : null
+            )}
         </div>
     );
 };
