9dc25dc2e49d53182e21b1c429fb1b0fc78b3fc6
aadnekar <aadne.karstad@gmail.com>
2023-03-09 11:18:25 +0100

    Generate types

--- typings/generated/src/App/appLayout.d.ts
+++ typings/generated/src/App/appLayout.d.ts
@@ -3,7 +3,7 @@ interface PaneSpec {
     name: string;
     Main: unknown;
 }
-export declare const reducer: import("redux").Reducer<AppLayout, import("redux").AnyAction>, setCurrentPane: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, string>, setPanes: import("@reduxjs/toolkit").ActionCreatorWithPayload<PaneSpec[], string>, toggleLogVisible: import("@reduxjs/toolkit").ActionCreatorWithoutPayload<string>, toggleSidePanelVisible: import("@reduxjs/toolkit").ActionCreatorWithoutPayload<string>;
+export declare const reducer: import("redux").Reducer<AppLayout, import("redux").AnyAction>, setCurrentPane: import("@reduxjs/toolkit").ActionCreatorWithPayload<number, string>, setPanes: import("@reduxjs/toolkit").ActionCreatorWithPayload<PaneSpec[], string>, toggleLogVisible: import("@reduxjs/toolkit").ActionCreatorWithoutPayload<string>, toggleSidePanelVisible: import("@reduxjs/toolkit").ActionCreatorWithoutPayload<string>, switchToNextPane: import("@reduxjs/toolkit").ActionCreatorWithoutPayload<string>, switchToPreviousPane: import("@reduxjs/toolkit").ActionCreatorWithoutPayload<string>;
 export declare const isSidePanelVisible: (state: RootState) => boolean;
 export declare const isLogVisible: (state: RootState) => boolean;
 export declare const paneNames: (state: RootState) => string[];
