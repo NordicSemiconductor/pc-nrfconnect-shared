a9aa492f01079c4a01ecce2f1c16efc5bbfd3da7
Chun Fan <4866953+chunfantasy@users.noreply.github.com>
2023-03-13 13:27:48 +0100

    Update according to code review

--- src/Steppers/Steppers.tsx
+++ src/Steppers/Steppers.tsx
@@ -10,13 +10,12 @@ import classNames from '../utils/classNames';
 
 import './steppers.scss';
 
+export type StepState = 'active' | 'success' | 'warn' | 'fail';
+
 export type Step = {
     title: string;
     caption?: string;
-    active?: boolean;
-    success?: boolean;
-    warn?: boolean;
-    fail?: boolean;
+    state?: StepState;
 };
 
 export interface Steppers {
@@ -27,35 +26,38 @@ export interface Steppers {
 const Steppers = ({ title, steps }: Steppers) => (
     <>
         {title && <div>{title}</div>}
-        {steps.map(step => {
-            console.log(step);
-            return (
-                <div
-                    key={step.title}
-                    className={classNames(
-                        'step',
-                        step.active && 'step-active',
-                        step.success && 'step-success',
-                        step.fail && 'step-fail',
-                        step.warn && 'step-warn'
-                    )}
-                >
-                    <div>
-                        <div className="circle">
-                            {step.active && <div className="loading" />}
-                            {step.success && <div className="check-mark" />}
-                            {step.fail && <div className="cross-mark" />}
-                            {step.warn && <div className="warning-mark" />}
-                        </div>
-                        <div className="step-line" />
+        {steps.map(step => (
+            <div
+                key={step.title}
+                className={classNames(
+                    'step',
+                    step.state === 'active' && 'step-active',
+                    step.state === 'success' && 'step-success',
+                    step.state === 'fail' && 'step-fail',
+                    step.state === 'warn' && 'step-warn'
+                )}
+            >
+                <div>
+                    <div className="circle">
+                        {step.state === 'active' && <div className="loading" />}
+                        {step.state === 'success' && (
+                            <div className="check-mark" />
+                        )}
+                        {step.state === 'fail' && (
+                            <div className="cross-mark" />
+                        )}
+                        {step.state === 'warn' && (
+                            <div className="warning-mark" />
+                        )}
                     </div>
-                    <div>
-                        <div className="title">{step.title}</div>
-                        <div className="caption">{step.caption}</div>
-                    </div>
+                    <div className="line" />
                 </div>
-            );
-        })}
+                <div>
+                    <div className="title">{step.title}</div>
+                    <div className="caption">{step.caption}</div>
+                </div>
+            </div>
+        ))}
     </>
 );
 
