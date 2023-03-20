1de03cbb9a0631cd153b83d844cd13135c17b01d
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-03-20 10:09:32 +0100

    feat: export getPersisted/persist-SerialPortOptions functions
    Signed-off-by: Pascal Pickel <Pascal.Pickel@nordicsemi.no>

--- src/index.ts
+++ src/index.ts
@@ -75,13 +75,16 @@ export { isLoggingVerbose } from './Log/logSlice';
 
 export {
     getAppSpecificStore as getPersistentStore,
-    persistSerialPort,
-    getPersistedSerialPort,
     persistTerminalSettings,
     getPersistedTerminalSettings,
+    getPersistedSerialPortOptions,
 } from './utils/persistentStore';
 
-export { selectedDevice, getReadbackProtection } from './Device/deviceSlice';
+export {
+    selectedDevice,
+    getReadbackProtection,
+    persistSerialPortOptions,
+} from './Device/deviceSlice';
 export {
     setWaitForDevice,
     clearWaitForDevice,
