3e58546dd7a563c130e33a6bd265652c7507f5cd
aadnekar <aadne.karstad@gmail.com>
2023-05-03 11:40:21 +0200

    Update auto generated types

--- typings/generated/src/SerialPort/ConflictingSettingsDialog.d.ts
+++ typings/generated/src/SerialPort/ConflictingSettingsDialog.d.ts
@@ -3,12 +3,11 @@ import type { AutoDetectTypes } from '@serialport/bindings-cpp';
 import { SerialPortOpenOptions } from 'serialport';
 import { SerialPort } from './SerialPort';
 interface ConflictingSettingsDialog {
-    appName?: string;
     isVisible: boolean;
     onOverwrite: () => void;
     onCancel: () => void;
     localSettings: SerialPortOpenOptions<AutoDetectTypes>;
     setSerialPortCallback: (serialPort: SerialPort) => void;
 }
-declare const ConflictingSettingsDialog: ({ appName, isVisible, onOverwrite, onCancel, localSettings, setSerialPortCallback, }: ConflictingSettingsDialog) => JSX.Element;
+declare const ConflictingSettingsDialog: ({ isVisible, onOverwrite, onCancel, localSettings, setSerialPortCallback, }: ConflictingSettingsDialog) => JSX.Element;
 export default ConflictingSettingsDialog;
