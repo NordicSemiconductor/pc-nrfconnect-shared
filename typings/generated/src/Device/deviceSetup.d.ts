d15c18f80fd810ce1fbd594d75366742d732bb93
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-05-22 10:04:44 +0200

    Fix types

--- typings/generated/src/Device/deviceSetup.d.ts
+++ typings/generated/src/Device/deviceSetup.d.ts
@@ -33,8 +33,8 @@ export interface DeviceSetup {
     deviceSetups: IDeviceSetup[];
     needSerialport: boolean;
     allowCustomDevice?: boolean;
-    booleanDialogText?: string;
-    choiceDialogText?: string;
+    confirmMessage?: string;
+    choiceMessage?: string;
 }
 export declare const prepareDevice: (device: Device, deviceSetupConfig: DeviceSetup, onSuccess: (device: Device) => void, onFail: (reason?: unknown) => void, checkCurrentFirmwareVersion?: boolean, requireUserConfirmation?: boolean) => (dispatch: TDispatch) => Promise<void>;
 export declare const setupDevice: (device: Device, deviceSetup: DeviceSetup, releaseCurrentDevice: () => void, onDeviceIsReady: (device: Device) => void, doDeselectDevice: () => void) => (dispatch: TDispatch, getState: () => RootState) => void;
