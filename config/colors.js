e41f046fc53c57026d5300e86f57bbbb17e5ee17
Jonas Fritzke Emdal <jonas.emdal@nordicsemi.no>
2023-06-19 11:25:08 +0200

    Inline color values

--- config/colors.js
+++ config/colors.js
@@ -6,43 +6,32 @@
 
 const nordicBlue = '#00a9ce';
 const blueSlate = '#0033a0';
-const primary = nordicBlue;
-const secondary = 'white';
-const accent = blueSlate;
-const gray = {
-    50: '#eceff1',
-    100: '#cfd8dc',
-    200: '#b0bec5',
-    300: '#90a4ae',
-    400: '#78909c',
-    500: '#607d8b',
-    600: '#546e7a',
-    700: '#455a64',
-    800: '#37474f',
-    900: '#263238',
-};
-const red = '#f44336';
-const indigo = '#3f51b5';
-const amber = '#ffc107';
-const purple = '#9c27b0';
-const green = '#4caf50';
-const deepPurple = '#673ab7';
-const orange = '#ff9800';
-const lime = '#cddc39';
-const pink = '#e91e63';
 
 module.exports = {
-    primary,
-    secondary,
-    accent,
-    gray,
-    red,
-    indigo,
-    amber,
-    purple,
-    green,
-    deepPurple,
-    orange,
-    lime,
-    pink,
+    nordicBlue: '#00a9ce',
+    blueSlate: '#0033a0',
+    primary: nordicBlue,
+    secondary: 'white',
+    accent: blueSlate,
+    gray: {
+        50: '#eceff1',
+        100: '#cfd8dc',
+        200: '#b0bec5',
+        300: '#90a4ae',
+        400: '#78909c',
+        500: '#607d8b',
+        600: '#546e7a',
+        700: '#455a64',
+        800: '#37474f',
+        900: '#263238',
+    },
+    red: '#f44336',
+    indigo: '#3f51b5',
+    amber: '#ffc107',
+    purple: '#9c27b0',
+    green: '#4caf50',
+    deepPurple: '#673ab7',
+    orange: '#ff9800',
+    lime: '#cddc39',
+    pink: '#e91e63',
 };
