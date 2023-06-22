be61c375690b6996f85b89e096c56ed3fd5b0e21
Jonas Fritzke Emdal <jonas.emdal@nordicsemi.no>
2023-06-22 16:30:13 +0200

    Generate those sturdy old types again.

--- typings/generated/test/testrenderer.d.ts
+++ typings/generated/test/testrenderer.d.ts
@@ -1,5 +1,5 @@
 import React from 'react';
 import { Action, Reducer } from 'redux';
-export declare const testRendererForApps: (appReducer?: Reducer) => (element: React.ReactElement, actions?: Action[]) => import("@testing-library/react").RenderResult<typeof import("@testing-library/dom/types/queries")>;
-declare const _default: (element: React.ReactElement, actions?: Action[], appReducer?: Reducer) => import("@testing-library/react").RenderResult<typeof import("@testing-library/dom/types/queries")>;
+export declare const testRendererForApps: (appReducer?: Reducer) => (element: React.ReactElement, actions?: Action[]) => import("@testing-library/react").RenderResult<typeof import("@testing-library/dom/types/queries"), HTMLElement, HTMLElement>;
+declare const _default: (element: React.ReactElement, actions?: Action[], appReducer?: Reducer) => import("@testing-library/react").RenderResult<typeof import("@testing-library/dom/types/queries"), HTMLElement, HTMLElement>;
 export default _default;
