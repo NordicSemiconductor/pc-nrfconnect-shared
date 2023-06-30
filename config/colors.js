7ca593d4e085116d9a6b03b68f2c06cee1a7f94a
Jonas Fritzke Emdal <jonas.emdal@nordicsemi.no>
2023-06-30 15:44:45 +0200

    Reuse existing color definition

--- config/colors.js
+++ config/colors.js
@@ -3,35 +3,20 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
+const flattenedColors = require('../src/utils/colors').colors;
 
-const nordicBlue = '#00a9ce';
-const blueSlate = '#0033a0';
+const grays = Object.entries(flattenedColors)
+    .filter(([key]) => key.startsWith('gray'))
+    .map(([key, value]) => [key.replace('gray', ''), value]);
+
+const colors = Object.entries(flattenedColors).filter(
+    ([key]) => !key.startsWith('gray')
+);
 
 module.exports = {
-    nordicBlue: '#00a9ce',
-    blueSlate: '#0033a0',
-    primary: nordicBlue,
-    secondary: 'white',
-    accent: blueSlate,
+    ...Object.fromEntries(colors),
+
     gray: {
-        50: '#eceff1',
-        100: '#cfd8dc',
-        200: '#b0bec5',
-        300: '#90a4ae',
-        400: '#78909c',
-        500: '#607d8b',
-        600: '#546e7a',
-        700: '#455a64',
-        800: '#37474f',
-        900: '#263238',
+        ...Object.fromEntries(grays),
     },
-    red: '#f44336',
-    indigo: '#3f51b5',
-    amber: '#ffc107',
-    purple: '#9c27b0',
-    green: '#4caf50',
-    deepPurple: '#673ab7',
-    orange: '#ff9800',
-    lime: '#cddc39',
-    pink: '#e91e63',
 };
