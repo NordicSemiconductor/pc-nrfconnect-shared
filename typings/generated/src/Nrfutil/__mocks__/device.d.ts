7aa533e21a747130d91d583b83e27c6ce30b8a27
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-07-19 12:46:55 +0200

    fix: update types

--- typings/generated/src/Nrfutil/__mocks__/device.d.ts
+++ typings/generated/src/Nrfutil/__mocks__/device.d.ts
@@ -1,22 +1,20 @@
 /// <reference types="jest" />
-declare const getDeviceLib: () => Promise<{
-    program: jest.Mock<any, any, any>;
-    programBuffer: jest.Mock<any, any, any>;
-    erase: jest.Mock<any, any, any>;
-    recover: jest.Mock<any, any, any>;
-    reset: jest.Mock<any, any, any>;
-    getProtectionStatus: jest.Mock<any, any, any>;
-    setProtectionStatus: jest.Mock<any, any, any>;
-    fwInfo: jest.Mock<any, any, any>;
-    setMcuState: jest.Mock<any, any, any>;
-    coreInfo: jest.Mock<any, any, any>;
-    list: jest.Mock<{
-        stop: jest.Mock<any, any, any>;
-    }, [], any>;
-    firmwareRead: jest.Mock<any, any, any>;
-    onLogging: jest.Mock<any, any, any>;
-    setLogLevel: jest.Mock<any, any, any>;
-    setVerboseLogging: jest.Mock<any, any, any>;
-    getModuleVersion: jest.Mock<any, any, any>;
-}>;
-export default getDeviceLib;
+declare const program: jest.Mock<any, any, any>;
+declare const programBuffer: jest.Mock<any, any, any>;
+declare const erase: jest.Mock<any, any, any>;
+declare const recover: jest.Mock<any, any, any>;
+declare const reset: jest.Mock<any, any, any>;
+declare const getProtectionStatus: jest.Mock<any, any, any>;
+declare const setProtectionStatus: jest.Mock<any, any, any>;
+declare const getFwInfo: jest.Mock<any, any, any>;
+declare const setMcuState: jest.Mock<any, any, any>;
+declare const getCoreInfo: jest.Mock<any, any, any>;
+declare const list: jest.Mock<{
+    stop: jest.Mock<any, any, any>;
+}, [], any>;
+declare const firmwareRead: jest.Mock<any, any, any>;
+declare const onLogging: jest.Mock<any, any, any>;
+declare const setLogLevel: jest.Mock<any, any, any>;
+declare const setVerboseLogging: jest.Mock<any, any, any>;
+declare const getModuleVersion: jest.Mock<any, any, any>;
+export { program, programBuffer, erase, recover, reset, getProtectionStatus, setProtectionStatus, getFwInfo, setMcuState, getCoreInfo, list, firmwareRead, onLogging, setLogLevel, setVerboseLogging, getModuleVersion, };
