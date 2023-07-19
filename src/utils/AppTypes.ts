ce46c7587c43cb5f4f3444ef98730190ed005ab4
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2023-07-19 09:44:45 +0200

    refactor: improve readability of WithRequired type

--- src/utils/AppTypes.ts
+++ src/utils/AppTypes.ts
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
-export type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] };
+export type WithRequired<T, K extends keyof T> = T & Required<Pick<T, K>>;
 
 export interface SourceJson {
     name: string;
