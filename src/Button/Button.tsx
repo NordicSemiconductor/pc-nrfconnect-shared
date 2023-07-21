dca90fa2ab71979b51ed13ea1cf050c15a5b333d
kylebonnici <kylebonnici@hotmail.com>
2023-07-21 17:25:43 +0200

    Feat: remove unused classes

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -42,26 +42,26 @@ const Button: React.FC<ButtonProps> = ({
         <button
             type="button"
             id={id}
-            className={`tw-border ${classNames(
+            className={`${classNames(
                 large
                     ? 'tw-h-8 tw-px-4 tw-text-sm'
                     : 'tw-h-6 tw-px-2 tw-text-xs',
                 variant === 'primary' &&
-                    'tw-border-none tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700',
+                    'tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700',
                 variant === 'secondary' &&
-                    'tw-border-gray-700 tw-bg-white tw-text-gray-700  active:enabled:tw-bg-gray-50',
+                    'tw-border tw-border-gray-700 tw-bg-white tw-text-gray-700  active:enabled:tw-bg-gray-50',
                 variant === 'success' &&
-                    'tw-border-none tw-bg-green tw-text-white  active:enabled:tw-bg-green-700',
+                    'tw-bg-green tw-text-white  active:enabled:tw-bg-green-700',
                 variant === 'info' &&
-                    'tw-border-none tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700',
+                    'tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700',
                 variant === 'warning' &&
-                    'tw-border-none tw-bg-orange tw-text-white active:enabled:tw-bg-orange-700',
+                    'tw-bg-orange tw-text-white active:enabled:tw-bg-orange-700',
                 variant === 'danger' &&
-                    'tw-border-none tw-bg-red tw-text-white active:enabled:tw-bg-red-700',
+                    'tw-bg-red tw-text-white active:enabled:tw-bg-red-700',
                 variant === 'link' &&
-                    'tw-bg-transparent tw-border-none tw-p-0 tw-text-nordicBlue',
+                    'tw-bg-transparent tw-p-0 tw-text-nordicBlue',
                 variant === 'link-button' &&
-                    'tw-border-nordicBlue tw-bg-white tw-text-nordicBlue active:enabled:tw-bg-gray-50',
+                    'tw-border tw-border-nordicBlue tw-bg-white tw-text-nordicBlue active:enabled:tw-bg-gray-50',
                 className
             )}`}
             disabled={disabled}
