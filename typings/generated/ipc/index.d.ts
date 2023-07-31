3730cc7ca40669a72d8a44984869051686f9c1cb
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-07-31 22:13:04 +0200

    Clean outdated generated types

--- typings/generated/ipc/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * as rendererToMain from './infrastructure/rendererToMain';
-export * as mainToRenderer from './infrastructure/mainToRenderer';
-export type { App, AppSpec, AppWithError, DownloadableApp, InstalledDownloadableApp, LaunchableApp, LocalApp, SourceWithError, UninstalledDownloadableApp, WithdrawnApp, } from './apps';
-export type { OpenAppOptions } from './openWindow';
-export { LOCAL, OFFICIAL, allStandardSourceNames, type Source, type SourceName, type SourceUrl, } from './sources';
-export type { OverwriteOptions } from './serialPort';
-export type { AppInfo, AppVersions, PackageJson, SourceJson, UrlString, WithdrawnJson, } from './MetaFiles';
-//# sourceMappingURL=index.d.ts.map
