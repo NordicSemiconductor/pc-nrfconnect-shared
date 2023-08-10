ec43620874abc6a85ccf3b3a5c0ee2437aeb6e28
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-08-10 12:38:19 +0200

    Compile only required scripts
    Only `nordic-publish` needs to be compiled, we can run the rest with
    ts-node. But `nordic-publish` doesn`t need to be in `bin`, because we
    always run it explicitly by filename.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -11,20 +11,10 @@ const { existsSync } = require('fs');
 const { resolve } = require('path');
 const { build } = require('./esbuild-renderer');
 
-const scripts = [
-    'nordic-publish',
-    'check-app-properties',
-    'check-for-typescript',
-    'nrfconnect-license',
-];
-
-scripts.forEach(script => {
-    // compile scripts if they don't exist
-    if (!existsSync(`scripts/${script}.js`)) {
-        const command = `npx esbuild scripts/${script}.ts --bundle --outfile=scripts/${script}.js --platform=node --log-level=warning --minify`;
-        execSync(command, { encoding: 'utf-8' });
-    }
-});
+if (!existsSync(`scripts/nordic-publish.js`)) {
+    const command = `npx esbuild scripts/nordic-publish.ts --bundle --outfile=scripts/nordic-publish.js --platform=node --log-level=warning --minify`;
+    execSync(command, { encoding: 'utf-8' });
+}
 
 if (!existsSync(`dist/bootstrap.css`)) {
     build({
