0a6986befe3afb10d6eaf10d01f991868c6fabf8
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-08-11 10:59:24 +0200

    Feat: sandbox usage reporting

---
+++ nrfutil/nrfutilLogger.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+import winston from 'winston';
+
+let nrfutilLogger: winston.Logger | undefined;
+
+export const setNrfutilLogger = (logger: winston.Logger) => {
+    nrfutilLogger = logger;
+};
+
+export const getNrfutilLogger = () => nrfutilLogger;
