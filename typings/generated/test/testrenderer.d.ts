2f54b859412484a108328b8f812b2a1731328013
aadnekar <aadne.karstad@gmail.com>
2023-08-15 10:46:43 +0200

    Remove generated typings

--- typings/generated/test/testrenderer.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Action as SimpleAction, Reducer } from 'redux';
-import { AppThunk } from '../src/store';
-type Action = SimpleAction | AppThunk<never>;
-export declare const testRendererForApps: (appReducer?: Reducer) => (element: React.ReactElement, actions?: Action[]) => import("@testing-library/react").RenderResult<typeof import("@testing-library/dom/types/queries")>;
-declare const _default: (element: React.ReactElement, actions?: Action[], appReducer?: Reducer) => import("@testing-library/react").RenderResult<typeof import("@testing-library/dom/types/queries")>;
-export default _default;
-//# sourceMappingURL=testrenderer.d.ts.map
