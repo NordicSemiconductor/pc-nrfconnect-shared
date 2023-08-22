6a2a8dda3087b0666e0258c9415bb9e9ac4ef630
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-08-22 12:22:19 +0200

    Refactor: Extract functions

--- scripts/check-app-properties.ts
+++ scripts/check-app-properties.ts
@@ -80,6 +80,51 @@ const checkMandatoryProperties = (packageJson: PackageJson) => {
     );
 };
 
+const checkNrfutilProperties = (packageJson: PackageJson) => {
+    const nrfutilModules = packageJson.nrfConnectForDesktop?.nrfutil;
+    if (nrfutilModules != null) {
+        Object.entries(nrfutilModules).forEach(
+            ([moduleName, moduleVersions]) => {
+                if (
+                    !Array.isArray(moduleVersions) ||
+                    moduleVersions.length === 0
+                ) {
+                    fail(
+                        `For each module in \`nrfConnectForDesktop.nrfutil\` in package.json at least one version must be specified, but for \`${moduleName}\` none was specified.`
+                    );
+                }
+            }
+        );
+    }
+};
+
+const checkRepoUrl = (packageJson: PackageJson) => {
+    if (!existsSync('./.git')) {
+        return;
+    }
+
+    const realGitUrl = execSync('git remote get-url origin', {
+        encoding: 'utf-8',
+    }).trimEnd();
+    const declaredGitUrl = packageJson.repository?.url;
+
+    const withoutPostfix = (gitUrl?: string) => gitUrl?.replace(/\.git$/, '');
+
+    const withoutProtocol = (gitUrl?: string) =>
+        gitUrl
+            ?.replace(/^git@github\.com:/, 'github.com/')
+            .replace(/^https:\/\//, '');
+
+    const stripped = (gitUrl?: string) =>
+        withoutProtocol(withoutPostfix(gitUrl));
+
+    if (stripped(realGitUrl) !== stripped(declaredGitUrl)) {
+        fail(
+            `package.json says the repository is located at \`${declaredGitUrl}\` but \`git remote get-url origin\` says it is at \`${realGitUrl}\`.`
+        );
+    }
+};
+
 const checkOptionalProperties = (packageJson: PackageJson) => {
     if (propertyIsMissing(packageJson)('homepage')) {
         warn('Please provide a property `homepage` in package.json.');
@@ -90,47 +135,13 @@ const checkOptionalProperties = (packageJson: PackageJson) => {
             'Please provide a property `nrfConnectForDesktop.html` in package.json'
         );
     } else {
-        const nrfutilModules = packageJson.nrfConnectForDesktop?.nrfutil;
-        if (nrfutilModules != null) {
-            Object.entries(nrfutilModules).forEach(
-                ([moduleName, moduleVersions]) => {
-                    if (
-                        !Array.isArray(moduleVersions) ||
-                        moduleVersions.length === 0
-                    ) {
-                        fail(
-                            `For each module in \`nrfConnectForDesktop.nrfutil\` in package.json at least one version must be specified, but for \`${moduleName}\` none was specified.`
-                        );
-                    }
-                }
-            );
-        }
+        checkNrfutilProperties(packageJson);
     }
 
     if (propertyIsMissing(packageJson)('repository.url')) {
         warn('Please provide a property `repository.url` in package.json.');
-    } else if (existsSync('./.git')) {
-        const realGitUrl = execSync('git remote get-url origin', {
-            encoding: 'utf-8',
-        }).trimEnd();
-        const declaredGitUrl = packageJson.repository?.url;
-
-        const withoutPostfix = (gitUrl?: string) =>
-            gitUrl?.replace(/\.git$/, '');
-
-        const withoutProtocol = (gitUrl?: string) =>
-            gitUrl
-                ?.replace(/^git@github\.com:/, 'github.com/')
-                .replace(/^https:\/\//, '');
-
-        const stripped = (gitUrl?: string) =>
-            withoutProtocol(withoutPostfix(gitUrl));
-
-        if (stripped(realGitUrl) !== stripped(declaredGitUrl)) {
-            fail(
-                `package.json says the repository is located at \`${declaredGitUrl}\` but \`git remote get-url origin\` says it is at \`${realGitUrl}\`.`
-            );
-        }
+    } else {
+        checkRepoUrl(packageJson);
     }
 };
 
