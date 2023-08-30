b74fcda95eb51e0620b944fc0a28bc39a68f7cb1
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-08-30 14:36:55 +0200

    Feat: change return type

--- nrfutil/device/uartWrite.ts
+++ nrfutil/device/uartWrite.ts
@@ -6,7 +6,7 @@
 
 import { Progress } from '../sandboxTypes';
 import {
-    deviceSingleTaskEndOperationVoid,
+    deviceSingleTaskEndOperation,
     NrfutilDeviceWithSerialnumber,
 } from './common';
 
@@ -24,7 +24,7 @@ export default async (
         args.push(vCom.toString());
     }
 
-    await deviceSingleTaskEndOperationVoid(
+    await deviceSingleTaskEndOperation<string>(
         device,
         'uart-write',
         onProgress,
