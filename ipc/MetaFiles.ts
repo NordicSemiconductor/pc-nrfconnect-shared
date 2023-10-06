0255dba93487b251248057588e840d9837f3dc41
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-10-06 17:25:56 +0200

    Enable package.json validation
    Not yet used anywhere, but this will be added in the next commits.

--- ipc/MetaFiles.ts
+++ ipc/MetaFiles.ts
@@ -4,7 +4,9 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import { DevicePCA } from '../src/Device/deviceInfo/deviceInfo';
+import { z } from 'zod';
+
+import { knownDevicePcas } from './device';
 
 export type UrlString = string;
 
@@ -40,49 +42,79 @@ export interface AppInfo {
     };
 }
 
-interface ObjectContainingOptionalStrings {
-    [index: string]: string | undefined;
-}
+const semver = z.string().regex(
+    // From https://semver.org
+    /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/,
+    'Is not a valid string for a semantic version'
+);
 
-interface NrfConnectForDesktop {
-    supportedDevices?: DevicePCA[];
-    nrfutil?: NrfutilModules;
-    html?: string;
-}
+const nrfutilModuleName = z.string();
+const nrfutilModuleVersion = semver;
 
-type SemverString = string;
+export type NrfutilModuleName = z.infer<typeof nrfutilModuleName>;
+export type NrfutilModuleVersion = z.infer<typeof nrfutilModuleVersion>;
 
-export type NrfutilModuleName = string;
-export type NrfutilModuleVersion = SemverString;
+const nrfModules = z.record(
+    nrfutilModuleName,
+    nrfutilModuleVersion.array().nonempty()
+);
 
-export interface NrfutilModules {
-    [name: NrfutilModuleName]: [
-        NrfutilModuleVersion,
-        ...NrfutilModuleVersion[]
-    ];
-}
+export type NrfutilModules = z.infer<typeof nrfModules>;
 
-export interface PackageJson {
-    name: string;
-    version: SemverString;
+const nrfConnectForDesktop = z.object({
+    supportedDevices: z.enum(knownDevicePcas).array().nonempty().optional(),
+    nrfutil: nrfModules.optional(),
+    html: z.string(),
+});
 
-    // Several optional properties
-    author?: string;
-    bin?: ObjectContainingOptionalStrings | string;
-    dependencies?: ObjectContainingOptionalStrings;
-    description?: string;
-    homepage?: UrlString;
-    devDependencies?: ObjectContainingOptionalStrings;
-    displayName?: string;
-    engines?: ObjectContainingOptionalStrings;
-    nrfConnectForDesktop?: NrfConnectForDesktop;
-    files?: readonly string[];
-    license?: string;
-    main?: string;
-    peerDependencies?: ObjectContainingOptionalStrings;
-    repository?: {
-        type: string;
-        url: UrlString;
-    };
-    scripts?: ObjectContainingOptionalStrings;
-}
+const recordOfOptionalStrings = z.record(z.string().optional());
+
+const engines = recordOfOptionalStrings.and(
+    z.object({ nrfconnect: z.string() })
+);
+
+const packageJson = z.object({
+    name: z.string(),
+    version: semver,
+
+    author: z.string().optional(),
+    bin: z.string().or(recordOfOptionalStrings).optional(),
+    dependencies: recordOfOptionalStrings.optional(),
+    description: z.string(),
+    homepage: z.string().url().optional(),
+    devDependencies: recordOfOptionalStrings.optional(),
+    displayName: z.string(),
+    engines,
+    nrfConnectForDesktop,
+    files: z.string().array().optional(),
+    license: z.string().optional(),
+    main: z.string().optional(),
+    peerDependencies: recordOfOptionalStrings.optional(),
+    repository: z
+        .object({
+            type: z.string(),
+            url: z.string().url(),
+        })
+        .optional(),
+    scripts: recordOfOptionalStrings.optional(),
+});
+
+export const parsePackageJson = (packageJsonContents: string) =>
+    packageJson.safeParse(JSON.parse(packageJsonContents));
+
+export type PackageJson = z.infer<typeof packageJson>;
+
+// In the launcher we want to handle that the html in nrfConnectForDesktop
+// can also be undefined, so there we need to use this variant
+const legacyNrfConnectForDesktop = nrfConnectForDesktop.partial({ html: true });
+
+const legacyPackageJson = packageJson.merge(
+    z.object({
+        nrfConnectForDesktop: legacyNrfConnectForDesktop,
+    })
+);
+
+export const parseLegacyPackageJson = (packageJsonContents: string) =>
+    legacyPackageJson.safeParse(JSON.parse(packageJsonContents));
+
+export type LegacyPackageJson = z.infer<typeof legacyPackageJson>;
