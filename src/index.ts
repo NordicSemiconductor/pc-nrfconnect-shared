9b618faf9ccd05ae3c8f208c196028035c185712
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2024-01-16 11:18:48 +0100

    feat: export `openFile`

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,7 @@ export {
     getUserDataDir,
 } from './utils/appDirs';
 
-export { openUrl } from './utils/open';
+export { openUrl, openFile } from './utils/open';
 export { default as systemReport } from './utils/systemReport';
 
 export { default as usageData } from './utils/usageData';
