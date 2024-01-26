79ebd218c683a62579c8286f5a8a59bebdbeddcc
aadnekar <aadne.karstad@gmail.com>
2024-01-26 12:08:37 +0100

    Linter should ignore resources

--- config/eslintrc.js
+++ config/eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     ],
     ignorePatterns: [
         'doc/docs/',
+        'resources/',
         'package-lock.json',
         'scripts/nordic-publish.js',
         'typings/generated',
