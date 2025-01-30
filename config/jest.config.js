cfbb6b9743a789fb99f45324381f0be37e6771f2
Marko Schulz <Marko.Schulz@nordicsemi.no>
2025-01-30 16:19:20 +0100

    Use existing empty mock

--- config/jest.config.js
+++ config/jest.config.js
@@ -13,11 +13,10 @@ module.exports = (disabledMocks = []) => ({
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': `${mockDir}/fileMock.ts`,
         '\\.(css|scss)$': `${mockDir}/emptyMock.ts`,
-        'pc-nrfjprog-js|nrf-device-setup|usb': `${mockDir}/emptyMock.ts`,
+        'pc-nrfjprog-js|nrf-device-setup|usb|applicationinsights': `${mockDir}/emptyMock.ts`,
         '^electron$': `${mockDir}/electronMock.ts`,
         '^electron-store$': `${mockDir}/electronStoreMock.ts`,
         '@electron/remote': `${mockDir}/remoteMock.ts`,
-        applicationinsights: `${mockDir}/applicationinsightsMock.ts`,
         'react-markdown': require.resolve(
             'react-markdown/react-markdown.min.js'
         ),
--- mocks/applicationinsightsMock.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2025 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
- */
-
-// used solely for applicationinsights v2.x.x package to remove warning during tests
-export default {};
