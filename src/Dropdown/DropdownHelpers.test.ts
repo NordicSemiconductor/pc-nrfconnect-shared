f3eca4dae710178d9fcfee36ae43f8eb92d83e03
Marko Schulz <Marko.Schulz@nordicsemi.no>
2025-07-17 17:17:42 +0200

    Extract repeated test data

--- src/Dropdown/DropdownHelpers.test.ts
+++ src/Dropdown/DropdownHelpers.test.ts
@@ -10,37 +10,35 @@ import {
     getSelectedDropdownItem,
 } from './DropdownHelpers';
 
+const itemList = [
+    { label: 'foo', value: 'foo' },
+    { label: 'bar', value: 'bar' },
+];
+
 describe('getSelectedDropdownItem', () => {
     it('returns the first item if value is undefined', () => {
-        const itemList = [{ label: 'foo', value: 'foo' }];
         expect(getSelectedDropdownItem(itemList, undefined)).toBe(itemList[0]);
     });
 
     it('returns the first item if value is not found', () => {
-        const itemList = [{ label: 'foo', value: 'foo' }];
-        expect(getSelectedDropdownItem(itemList, 'bar')).toBe(itemList[0]);
+        expect(getSelectedDropdownItem(itemList, 'unknown')).toBe(itemList[0]);
     });
 
     it('returns the item with the correct value', () => {
-        const itemList = [
-            { label: 'foo', value: 'foo' },
-            { label: 'bar', value: 'bar' },
-        ];
         expect(getSelectedDropdownItem(itemList, 'bar')).toBe(itemList[1]);
     });
 
     it('returns the item with the correct value when value is a boolean', () => {
-        const itemList = [
+        const booleanList = [
             { label: 'on', value: 'on' },
             { label: 'off', value: 'off' },
         ];
-        expect(getSelectedDropdownItem(itemList, true)).toBe(itemList[0]);
+        expect(getSelectedDropdownItem(booleanList, true)).toBe(booleanList[0]);
     });
 
     it('returns the notFound item if value is not found', () => {
-        const itemList = [{ label: 'foo', value: 'foo' }];
         const notFound = { label: 'not found', value: 'not found' };
-        expect(getSelectedDropdownItem(itemList, 'bar', notFound)).toBe(
+        expect(getSelectedDropdownItem(itemList, 'unknown', notFound)).toBe(
             notFound
         );
     });
