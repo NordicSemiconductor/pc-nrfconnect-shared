fde955f5d467ba332cb372d0da617494b7ff21bd
Marko Schulz <Marko.Schulz@nordicsemi.no>
2025-10-10 16:48:39 +0200

    Add current versions of Electron, Chrome, and Node
    This will enable build scripts to target the correct environment.

---
+++ scripts/versions.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+export const electron = '32.1.2';
+export const chrome = '128.0.6613.162';
+export const node = '20.17.0';
+
+export const chromeWithoutBuild = chrome.replace(/^(\d+\.\d+\.\d+).*$/, '$1');
