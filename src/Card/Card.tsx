fcb117945a5a6b498dd9358c4ab9c1bee1bd67a7
maeldroem <maeldroem@proton.me>
2026-03-06 09:26:29 +0100

    Changed Card component to fully use Tailwind

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -5,22 +5,30 @@
  */
 
 import React from 'react';
-import Card from 'react-bootstrap/Card';
 
-import styles from './card.module.scss';
+import classNames from '../utils/classNames';
+import flatstr from '../utils/flatstr';
 
-type NrfCardProps = {
+export interface CardProps {
     children: React.ReactNode;
     title: React.ReactNode;
-};
+    className?: string;
+}
 
-export default ({ children, title }: NrfCardProps) => (
-    <Card className={styles.card}>
-        <Card.Header className={styles.header}>
-            <Card.Title>
-                <span className={styles.title}>{title}</span>
-            </Card.Title>
-        </Card.Header>
-        <Card.Body className={styles.body}>{children}</Card.Body>
-    </Card>
+export default ({ children, title, className }: CardProps) => (
+    <article
+        className={classNames(
+            // prettier-ignore
+            // Prettier has a bugged rule. Prettier doesn't care about multiline strings,
+            // yet outputs an error on multiline strings in jsx/tsx files
+            flatstr`tw-preflight tw-relative tw-flex tw-flex-1 tw-flex-col tw-gap-4 tw-break-words tw-border
+            tw-border-solid tw-border-black tw-border-opacity-10 tw-bg-white tw-px-4 tw-pb-4`,
+            className,
+        )}
+    >
+        <header className="tw-border-b tw-border-solid tw-border-b-black tw-border-opacity-10 tw-py-4 tw-text-center">
+            <h3 className="tw-text-base tw-font-medium">{title}</h3>
+        </header>
+        <div className="tw-flex tw-flex-auto">{children}</div>
+    </article>
 );
