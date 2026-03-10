7366d6617af6296c36de9a530d74e87cff826425
maeldroem <maeldroem@proton.me>
2026-03-10 16:56:57 +0100

    Improved Button component's code structure and added primary-outline variant

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -10,6 +10,7 @@ import classNames from '../utils/classNames';
 
 export type ButtonVariants =
     | 'primary'
+    | 'primary-outline'
     | 'secondary'
     | 'success'
     | 'info'
@@ -38,37 +39,61 @@ const Button: React.FC<ButtonProps> = ({
     disabled = false,
     title,
     size = 'sm',
-}) => (
-    <button
-        type="button"
-        id={id}
-        className={`${classNames(
-            'tw-preflight',
-            size === 'sm' && 'tw-h-6 tw-px-2 tw-text-xs',
-            size === 'lg' && 'tw-h-8 tw-px-4 tw-text-sm',
-            size === 'xl' && 'tw-h-8 tw-px-4 tw-text-base',
-            variant === 'primary' &&
-                'tw-border tw-border-transparent tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700',
-            variant === 'secondary' &&
-                'tw-border tw-border-gray-700 tw-bg-white tw-text-gray-700 active:enabled:tw-bg-gray-50',
-            variant === 'success' &&
-                'tw-border tw-border-transparent tw-bg-green tw-text-white active:enabled:tw-bg-green-700',
-            variant === 'info' &&
-                'tw-border tw-border-transparent tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700',
-            variant === 'warning' &&
-                'tw-border tw-border-transparent tw-bg-orange tw-text-white active:enabled:tw-bg-orange-700',
-            variant === 'danger' &&
-                'tw-border tw-border-transparent tw-bg-red tw-text-white active:enabled:tw-bg-red-700',
-            variant === 'link-button' &&
-                'tw-border tw-border-nordicBlue tw-bg-white tw-text-nordicBlue active:enabled:tw-bg-gray-50',
-            className,
-        )}`}
-        disabled={disabled}
-        onClick={onClick}
-        title={title}
-    >
-        {children}
-    </button>
-);
+}) => {
+    // Rust-style assignment from `if`/`match`
+    const sizeStyles = (() => {
+        switch (size) {
+            case 'sm':
+                return 'tw-h-6 tw-px-2 tw-text-xs';
+            case 'lg':
+                return 'tw-h-8 tw-px-4 tw-text-sm';
+            case 'xl':
+                return 'tw-h-8 tw-px-4 tw-text-base';
+            default:
+                return '';
+        }
+    })();
+    const variantStyles = (() => {
+        // TODO: put long strings on multiple lines using flatstr
+        switch (variant) {
+            case 'primary':
+                return 'tw-border tw-border-transparent tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700';
+            case 'primary-outline':
+                return 'tw-border tw-border-nordicBlue tw-bg-white tw-text-nordicBlue active:enabled:tw-bg-nordicBlue-50';
+            case 'secondary':
+                return 'tw-border tw-border-gray-700 tw-bg-white tw-text-gray-700 active:enabled:tw-bg-gray-50';
+            case 'success':
+                return 'tw-border tw-border-transparent tw-bg-green tw-text-white active:enabled:tw-bg-green-700';
+            case 'info':
+                return 'tw-border tw-border-transparent tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700';
+            case 'warning':
+                return 'tw-border tw-border-transparent tw-bg-orange tw-text-white active:enabled:tw-bg-orange-700';
+            case 'danger':
+                return 'tw-border tw-border-transparent tw-bg-red tw-text-white active:enabled:tw-bg-red-700';
+            case 'link-button':
+                return 'tw-border tw-border-nordicBlue tw-bg-white tw-text-nordicBlue active:enabled:tw-bg-gray-50';
+            default:
+                return '';
+        }
+    })();
+
+    return (
+        <button
+            type="button"
+            id={id}
+            className={`${classNames(
+                'tw-preflight',
+                sizeStyles,
+                variantStyles,
+                className,
+            )}`}
+            disabled={disabled}
+            onClick={onClick}
+            title={title}
+        >
+            {children}
+        </button>
+    );
+};
 
 export default Button;
