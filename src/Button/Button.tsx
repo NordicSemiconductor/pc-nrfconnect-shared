fd18d156084f394854c5911447985e569a943f88
maeldroem <maeldroem@proton.me>
2026-03-17 09:15:49 +0100

    Reverted to using && one-liner for button size and variant styles

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -40,58 +40,39 @@ const Button: React.FC<ButtonProps> = ({
     variant,
     size = 'sm',
     ...attrs
-}) => {
-    // Rust-style assignment from `if`/`match`
-    const sizeStyles = (() => {
-        switch (size) {
-            case 'sm':
-                return 'tw-h-6 tw-px-2 tw-text-xs';
-            case 'lg':
-                return 'tw-h-8 tw-px-4 tw-text-sm';
-            case 'xl':
-                return 'tw-h-8 tw-px-4 tw-text-base';
-            default:
-                return '';
-        }
-    })();
-    const variantStyles = (() => {
-        // TODO: put long strings on multiple lines using flatstr
-        switch (variant) {
-            case 'primary':
-                return 'tw-border tw-border-transparent tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700';
-            case 'primary-outline':
-                return 'tw-border tw-border-nordicBlue tw-bg-white tw-text-nordicBlue active:enabled:tw-bg-nordicBlue-50';
-            case 'secondary':
-                return 'tw-border tw-border-gray-700 tw-bg-white tw-text-gray-700 active:enabled:tw-bg-gray-50';
-            case 'success':
-                return 'tw-border tw-border-transparent tw-bg-green tw-text-white active:enabled:tw-bg-green-700';
-            case 'info':
-                return 'tw-border tw-border-transparent tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700';
-            case 'warning':
-                return 'tw-border tw-border-transparent tw-bg-orange tw-text-white active:enabled:tw-bg-orange-700';
-            case 'danger':
-                return 'tw-border tw-border-transparent tw-bg-red tw-text-white active:enabled:tw-bg-red-700';
-            case 'link-button':
-                return 'tw-border tw-border-nordicBlue tw-bg-white tw-text-nordicBlue active:enabled:tw-bg-gray-50';
-            default:
-                return '';
-        }
-    })();
-
-    return (
-        <button
-            type="button"
-            className={`${classNames(
-                'tw-preflight',
-                sizeStyles,
-                variantStyles,
-                className,
-            )}`}
-            {...attrs}
-        >
-            {children}
-        </button>
-    );
-};
+}) => (
+    // Ideally we'd use switch-case statements beforehand but that'd disable
+    // Tailwind utility classes' validation
+    // Also, flatstr should be used to respect line width
+    <button
+        type="button"
+        className={`${classNames(
+            'tw-preflight',
+            size === 'sm' && 'tw-h-6 tw-px-2 tw-text-xs',
+            size === 'lg' && 'tw-h-8 tw-px-4 tw-text-sm',
+            size === 'xl' && 'tw-h-8 tw-px-4 tw-text-base',
+            variant === 'primary' &&
+                'tw-border tw-border-transparent tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700',
+            variant === 'primary-outline' &&
+                'tw-border tw-border-nordicBlue tw-bg-white tw-text-nordicBlue active:enabled:tw-bg-nordicBlue-50',
+            variant === 'secondary' &&
+                'tw-border tw-border-gray-700 tw-bg-white tw-text-gray-700 active:enabled:tw-bg-gray-50',
+            variant === 'success' &&
+                'tw-border tw-border-transparent tw-bg-green tw-text-white active:enabled:tw-bg-green-700',
+            variant === 'info' &&
+                'tw-border tw-border-transparent tw-bg-nordicBlue tw-text-white active:enabled:tw-bg-nordicBlue-700',
+            variant === 'warning' &&
+                'tw-border tw-border-transparent tw-bg-orange tw-text-white active:enabled:tw-bg-orange-700',
+            variant === 'danger' &&
+                'tw-border tw-border-transparent tw-bg-red tw-text-white active:enabled:tw-bg-red-700',
+            variant === 'link-button' &&
+                'tw-border tw-border-nordicBlue tw-bg-white tw-text-nordicBlue active:enabled:tw-bg-gray-50',
+            className,
+        )}`}
+        {...attrs}
+    >
+        {children}
+    </button>
+);
 
 export default Button;
