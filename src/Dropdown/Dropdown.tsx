45d5cae64503fc79022dd6233ee63a4062af2d1b
maeldroem <maeldroem@proton.me>
2026-03-17 15:25:55 +0100

    WIP migration of Dropdown component

--- src/Dropdown/Dropdown.tsx
+++ src/Dropdown/Dropdown.tsx
@@ -5,7 +5,7 @@
  */
 
 import React, { useState } from 'react';
-import FormLabel from 'react-bootstrap/FormLabel';
+import { nanoid } from '@reduxjs/toolkit';
 
 import classNames from '../utils/classNames';
 
@@ -16,8 +16,10 @@ export interface DropdownItem<T = string> {
     value: T;
 }
 
-export type DropdownProps<T> = {
-    id?: string;
+type PickedDropdownProps = 'ref' | 'className';
+
+export interface DropdownProps<T>
+    extends Pick<React.ComponentPropsWithRef<'div'>, PickedDropdownProps> {
     label?: React.ReactNode;
     defaultButtonLabel?: string;
     items: DropdownItem<T>[];
@@ -26,13 +28,11 @@ export type DropdownProps<T> = {
     minWidth?: boolean;
     disabled?: boolean;
     selectedItem: DropdownItem<T>;
-    numItemsBeforeScroll?: number;
-    className?: string;
+    // numItemsBeforeScroll?: number;
     size?: 'sm' | 'md';
-};
+}
 
-export default <T,>({
-    id,
+const Dropdown = <T,>({
     label,
     defaultButtonLabel = '',
     items,
@@ -41,10 +41,15 @@ export default <T,>({
     minWidth = false,
     disabled = false,
     selectedItem,
-    numItemsBeforeScroll = 0,
-    className = '',
+    // numItemsBeforeScroll = 0,
+    className,
     size = 'md',
 }: DropdownProps<T>) => {
+    // TODO: Replace with useId() after upgrading to newer React >=18.0
+    const dropdownId = nanoid();
+    const dropdownBtnId = `${dropdownId}-btn`;
+    const dropdownPopoverId = `${dropdownId}-dropdown`;
+
     const [isActive, setIsActive] = useState(false);
 
     const onClickItem = (item: DropdownItem<T>) => {
@@ -55,32 +60,43 @@ export default <T,>({
     return (
         <div
             className={classNames(
-                'tw-preflight tw-relative tw-text-xs',
-                minWidth ? '' : 'tw-w-full',
+                'tw-preflight tw-relative tw-flex tw-flex-col tw-gap-1 tw-text-xs',
+                !minWidth && 'tw-w-full',
                 className,
             )}
-            onBlur={event => {
-                if (!event.currentTarget.contains(event.relatedTarget)) {
-                    setIsActive(false);
-                }
-            }}
+            // onBlur={event => {
+            //     if (!event.currentTarget.contains(event.relatedTarget)) {
+            //         setIsActive(false);
+            //     }
+            // }}
         >
             {label && (
-                <FormLabel className="tw-mb-1 tw-text-xs">{label}</FormLabel>
+                <label className="tw-text-xs" htmlFor={dropdownBtnId}>
+                    {label}
+                </label>
             )}
             <button
-                id={id}
+                id={dropdownBtnId}
                 type="button"
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                // eslint-disable-next-line react/no-unknown-property
+                command="show-popover"
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                // eslint-disable-next-line react/no-unknown-property
+                commandfor={dropdownPopoverId}
                 className={classNames(
                     'tw-flex tw-items-center tw-justify-between tw-border-0',
-                    minWidth ? '' : 'tw-w-full',
+                    styles.anchor,
+                    !minWidth && 'tw-w-full',
                     transparentButtonBg
                         ? 'tw-bg-transparent'
                         : classNames(
                               'tw-bg-gray-700 tw-text-white',
-                              size === 'sm'
-                                  ? 'tw-h-6 tw-pl-2 tw-pr-1 tw-text-2xs'
-                                  : 'tw-h-8 tw-px-2',
+                              size === 'sm' &&
+                                  'tw-h-6 tw-pl-2 tw-pr-1 tw-text-2xs',
+                              size === 'md' && 'tw-h-8 tw-px-2',
                           ),
                 )}
                 onClick={() => setIsActive(!isActive)}
@@ -94,36 +110,50 @@ export default <T,>({
                 <span
                     className={`mdi mdi-chevron-down ${classNames(
                         isActive && 'tw-rotate-180',
-                        size === 'sm' ? 'tw-text-base' : 'tw-text-lg/none',
+                        size === 'sm' && 'tw-text-base',
+                        size === 'md' && 'tw-text-lg/none',
                     )}`}
                 />
             </button>
-            {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions -- We need an interactive handler as described below */}
-            <div
-                onMouseDown={ev => {
-                    // To prevent the dropdown from closing when users click on the scrollbar of the items
-                    ev.preventDefault();
-                }}
-                style={
-                    numItemsBeforeScroll > 0
-                        ? {
-                              maxHeight: `${numItemsBeforeScroll * 24}px`,
-                          }
-                        : {}
-                }
-                data-height={
-                    numItemsBeforeScroll > 0 &&
-                    items.length > numItemsBeforeScroll
-                }
-                className={`tw-text-while tw-absolute tw-z-10 tw-border-t-2 tw-border-solid tw-border-gray-600 tw-bg-gray-700 tw-p-0 ${classNames(
-                    styles.content,
-                    minWidth ? '' : 'tw-right-0 tw-w-full',
+            <dialog
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                // eslint-disable-next-line react/no-unknown-property
+                popover="hint"
+                id={dropdownPopoverId}
+                // onMouseDown={ev => {
+                //     // To prevent the dropdown from closing when users click on the scrollbar of the items
+                //     ev.preventDefault();
+                // }}
+                // style={
+                //     numItemsBeforeScroll > 0
+                //         ? {
+                //             maxHeight: `${numItemsBeforeScroll * 24}px`,
+                //         }
+                //         : {}
+                // }
+                // data-height={
+                //     numItemsBeforeScroll > 0 &&
+                //     items.length > numItemsBeforeScroll
+                // }
+                className={classNames(
+                    'tw-fixed tw-overflow-y-scroll tw-border-x-2 tw-border-t-2 tw-border-solid tw-border-gray-600 tw-bg-gray-700 tw-p-0 tw-text-white',
+                    styles.popover,
+                    !minWidth && 'tw-right-0 tw-w-full',
                     !isActive && 'tw-hidden',
-                )}`}
+                )}
             >
                 {items.map(item => (
                     <button
                         type="button"
+                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                        // @ts-ignore
+                        // eslint-disable-next-line react/no-unknown-property
+                        command="hide-popover"
+                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                        // @ts-ignore
+                        // eslint-disable-next-line react/no-unknown-property
+                        commandfor={dropdownPopoverId}
                         className={classNames(
                             'tw-clear-both tw-block tw-h-6 tw-w-full tw-whitespace-nowrap tw-border-0 tw-bg-transparent tw-px-2 tw-py-1 tw-text-left tw-font-normal tw-text-white hover:tw-bg-gray-600 focus:tw-bg-gray-600',
                             size === 'sm' && 'tw-text-2xs',
@@ -134,7 +164,9 @@ export default <T,>({
                         {item.label}
                     </button>
                 ))}
-            </div>
+            </dialog>
         </div>
     );
 };
+
+export default Dropdown;
