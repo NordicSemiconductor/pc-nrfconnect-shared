a544b772db3885030d0a96c94eb726e9c32c5c2e
maeldroem <maeldroem@proton.me>
2026-03-26 14:48:52 +0100

    Improved Modal and added popover component

---
+++ src/Popover/Popover.tsx
@@ -0,0 +1,72 @@
+import React, { type HTMLAttributes } from 'react';
+
+import Button, { type ButtonProps } from '../Button/Button';
+import classNames from '../utils/classNames';
+
+interface PopoverOpenButtonProps
+    extends Omit<ButtonProps, 'command' | 'commandfor'> {
+    popoverId: string;
+}
+
+type PopoverOpenButtonComponent = React.FC<
+    React.PropsWithChildren<PopoverOpenButtonProps>
+>;
+
+const PopoverOpenButton: PopoverOpenButtonComponent = ({
+    children,
+    popoverId,
+    ...attrs
+}) => (
+    <Button command="show-popover" commandfor={popoverId} {...attrs}>
+        {children}
+    </Button>
+);
+
+type PopoverClosingBehavior = 'normal' | 'hint' | 'manual';
+
+const popoverClosingBehaviorLookup: Record<
+    PopoverClosingBehavior,
+    HTMLAttributes<HTMLDialogElement>['popover']
+> = {
+    normal: 'auto',
+    hint: 'hint',
+    manual: 'manual',
+};
+
+interface PopoverProps
+    extends Pick<React.ComponentPropsWithRef<'dialog'>, 'ref' | 'className'> {
+    id: string;
+    closingBehavior?: PopoverClosingBehavior;
+}
+
+interface PopoverComponent
+    extends React.FC<React.PropsWithChildren<PopoverProps>> {
+    OpenButton: PopoverOpenButtonComponent;
+}
+
+const Popover: PopoverComponent = ({
+    children,
+    className,
+    id,
+    closingBehavior = 'normal',
+    ...attrs
+}) => {
+    const popoverType = popoverClosingBehaviorLookup[closingBehavior];
+
+    return (
+        <dialog
+            // eslint-disable-next-line react/no-unknown-property
+            popover={popoverType}
+            id={id}
+            className={classNames(
+                'tw-border tw-border-solid tw-border-black/10',
+                className,
+            )}
+            {...attrs}
+        >
+            {children}
+        </dialog>
+    );
+};
+
+Popover.OpenButton = PopoverOpenButton;
