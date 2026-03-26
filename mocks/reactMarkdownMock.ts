7e49c83c22925877c036cc03dde4f8505ccae163
maeldroem <maeldroem@proton.me>
2026-03-26 09:57:12 +0100

    Added react-markdown mock (passthrough)

---
+++ mocks/reactMarkdownMock.ts
@@ -0,0 +1,9 @@
+import Markdown, {
+    defaultUrlTransform,
+    MarkdownAsync,
+    MarkdownHooks,
+} from 'react-markdown';
+
+export { defaultUrlTransform, MarkdownAsync, MarkdownHooks };
+
+export default Markdown;
