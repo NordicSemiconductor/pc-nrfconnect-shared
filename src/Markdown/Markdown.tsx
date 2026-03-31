ae520128436405026356a2273f0a381f24af6678
maeldroem <maeldroem@proton.me>
2026-03-31 17:44:57 +0200

    Rewrite links in markdown to target _blank

--- src/Markdown/Markdown.tsx
+++ src/Markdown/Markdown.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import rehypeExternalLinks from 'rehype-external-links';
 
 import classNames from '../utils/classNames';
 
@@ -12,16 +13,24 @@ const Markdown: React.FC<MarkdownProps> = ({
     className,
     children,
     ...attrs
-}) => (
-    <div
-        className={classNames(
-            'tw-preflight tw-flex tw-flex-col tw-gap-4 [&_h1]:tw-text-lg [&_h1]:tw-font-medium [&_h2]:tw-text-lg [&_h2]:tw-font-medium [&_h3]:tw-text-lg [&_h3]:tw-font-medium [&_h4]:tw-text-lg [&_h4]:tw-font-medium [&_h5]:tw-text-lg [&_h5]:tw-font-medium [&_h6]:tw-text-lg [&_h6]:tw-font-medium [&_ol]:tw-list-inside [&_ol]:tw-list-decimal [&_ul]:tw-list-inside [&_ul]:tw-list-disc',
-            className,
-        )}
-        {...attrs}
-    >
-        <ReactMarkdown>{children}</ReactMarkdown>
-    </div>
-);
+}) => {
+    const externalLinksPlugin = rehypeExternalLinks({
+        target: '_blank',
+    });
+
+    return (
+        <div
+            className={classNames(
+                'tw-preflight tw-flex tw-flex-col tw-gap-4 [&_h1]:tw-text-lg [&_h1]:tw-font-medium [&_h2]:tw-text-lg [&_h2]:tw-font-medium [&_h3]:tw-text-lg [&_h3]:tw-font-medium [&_h4]:tw-text-lg [&_h4]:tw-font-medium [&_h5]:tw-text-lg [&_h5]:tw-font-medium [&_h6]:tw-text-lg [&_h6]:tw-font-medium [&_ol]:tw-list-inside [&_ol]:tw-list-decimal [&_ul]:tw-list-inside [&_ul]:tw-list-disc',
+                className,
+            )}
+            {...attrs}
+        >
+            <ReactMarkdown rehypePlugins={[externalLinksPlugin]}>
+                {children}
+            </ReactMarkdown>
+        </div>
+    );
+};
 
 export default Markdown;
