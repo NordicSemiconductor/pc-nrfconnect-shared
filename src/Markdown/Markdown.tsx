72674963a158306d2fdca4fa1eaa96a3bb73b065
maeldroem <maeldroem@proton.me>
2026-04-07 10:46:19 +0200

    Fixed rehype plugin
    See https://github.com/remarkjs/react-markdown/issues/927

--- src/Markdown/Markdown.tsx
+++ src/Markdown/Markdown.tsx
@@ -13,24 +13,21 @@ const Markdown: React.FC<MarkdownProps> = ({
     className,
     children,
     ...attrs
-}) => {
-    const externalLinksPlugin = rehypeExternalLinks({
-        target: '_blank',
-    });
-
-    return (
-        <div
-            className={classNames(
-                'tw-preflight tw-flex tw-flex-col tw-gap-4 [&_code]:tw-font-mono [&_code]:tw-text-pink [&_h1]:tw-text-lg [&_h1]:tw-font-medium [&_h2]:tw-text-lg [&_h2]:tw-font-medium [&_h3]:tw-text-lg [&_h3]:tw-font-medium [&_h4]:tw-text-lg [&_h4]:tw-font-medium [&_h5]:tw-text-lg [&_h5]:tw-font-medium [&_h6]:tw-text-lg [&_h6]:tw-font-medium [&_i]:tw-italic [&_ol]:tw-list-inside [&_ol]:tw-list-decimal [&_strong]:tw-font-bold [&_ul]:tw-list-inside [&_ul]:tw-list-disc',
-                className,
-            )}
-            {...attrs}
+}) => (
+    <div
+        className={classNames(
+            'tw-preflight tw-flex tw-flex-col tw-gap-4 [&_code]:tw-font-mono [&_code]:tw-text-pink [&_h1]:tw-text-lg [&_h1]:tw-font-medium [&_h2]:tw-text-lg [&_h2]:tw-font-medium [&_h3]:tw-text-lg [&_h3]:tw-font-medium [&_h4]:tw-text-lg [&_h4]:tw-font-medium [&_h5]:tw-text-lg [&_h5]:tw-font-medium [&_h6]:tw-text-lg [&_h6]:tw-font-medium [&_i]:tw-italic [&_ol]:tw-list-inside [&_ol]:tw-list-decimal [&_strong]:tw-font-bold [&_ul]:tw-list-inside [&_ul]:tw-list-disc',
+            className,
+        )}
+        {...attrs}
+    >
+        <ReactMarkdown
+            // See https://github.com/remarkjs/react-markdown/issues/927
+            rehypePlugins={[[rehypeExternalLinks, { target: '_blank' }]]}
         >
-            <ReactMarkdown rehypePlugins={[externalLinksPlugin]}>
-                {children}
-            </ReactMarkdown>
-        </div>
-    );
-};
+            {children}
+        </ReactMarkdown>
+    </div>
+);
 
 export default Markdown;
