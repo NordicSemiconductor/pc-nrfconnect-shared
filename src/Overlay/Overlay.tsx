751af8eda910cafd397a22750a865ede9163dbdf
maeldroem <maeldroem@proton.me>
2026-04-09 18:16:58 +0200

    Created context around Overlay — WIP

--- src/Overlay/Overlay.tsx
+++ src/Overlay/Overlay.tsx
@@ -4,11 +4,20 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import React, { useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 
 import './overlay.scss';
+import Popover from '../Popover/Popover';
+
+const isElementEnabled = (elem: React.ReactNode): boolean =>
+    !(
+        React.isValidElement(elem) &&
+        elem.props instanceof Object &&
+        'disabled' in elem.props &&
+        elem.props.disabled
+    );
 
 type OverlayBaseProps = Pick<
     React.ComponentPropsWithRef<'div'>,
@@ -17,11 +26,26 @@ type OverlayBaseProps = Pick<
 
 type OverlayBaseComponent = React.FC<React.PropsWithChildren<OverlayBaseProps>>;
 
-const OverlayBase: OverlayBaseComponent = ({ children, ...attrs }) => (
-    <div className="tw-cursor-help" {...attrs}>
-        {children}
-    </div>
-);
+const OverlayBase: OverlayBaseComponent = ({ children, ...attrs }) => {
+    const { setIsBaseHovered, setIsBaseEnabled } = useContext(OverlayContext);
+
+    setIsBaseEnabled?.(isElementEnabled(children));
+
+    return (
+        <div
+            className="tw-cursor-help"
+            onMouseEnter={() => {
+                setIsBaseHovered?.(true);
+            }}
+            onMouseLeave={() => {
+                setIsBaseHovered?.(false);
+            }}
+            {...attrs}
+        >
+            {children}
+        </div>
+    );
+};
 
 type OverlayOverlayProps = Pick<
     React.ComponentPropsWithRef<'div'>,
@@ -32,12 +56,26 @@ type OverlayOverlayComponent = React.FC<
     React.PropsWithChildren<OverlayOverlayProps>
 >;
 
-const OverlayOverlay: OverlayOverlayComponent = ({ children, ...attrs }) => (
-    <div {...attrs}>{children}</div>
-);
+const OverlayOverlay: OverlayOverlayComponent = ({ children, ...attrs }) => {
+    const { setIsOverlayHovered, showOverlay } = useContext(OverlayContext);
+
+    return (
+        <Popover
+            onMouseEnter={() => {
+                setIsOverlayHovered?.(true);
+            }}
+            onMouseLeave={() => {
+                setIsOverlayHovered?.(false);
+            }}
+            {...attrs}
+        >
+            {children}
+        </Popover>
+    );
+};
 
 type OverlayTriggerElem = 'base' | 'base-or-overlay';
-type OverlayTriggerRestraint = null | 'only-active';
+type OverlayTriggerRestraint = 'only-enabled';
 type OverlayPlacement =
     | 'top-left'
     | 'top'
@@ -65,16 +103,72 @@ interface OverlayComponent
     Overlay: OverlayOverlayComponent;
 }
 
+interface OverlayContextTy {
+    setIsBaseEnabled?: (isBaseEnabled: boolean) => void;
+    setIsBaseHovered?: (isBaseHovered: boolean) => void;
+    setIsOverlayHovered?: (isOverlayHovered: boolean) => void;
+    showOverlay: boolean;
+}
+
+const OverlayContext = createContext<OverlayContextTy>({});
+
 const Overlay: OverlayComponent = ({
     triggerElem = 'base',
-    triggerRestraint = 'only-active',
+    triggerRestraint = 'only-enabled',
     placement = 'bottom',
     children,
     ...attrs
 }) => {
     const [showOverlay, setShowOverlay] = useState<boolean>(false);
 
-    return <div {...attrs}>{children}</div>;
+    const [isBaseHovered, setIsBaseHovered] = useState<boolean>(false);
+    const [isOverlayHovered, setIsOverlayHovered] = useState<boolean>(false);
+    const [isBaseEnabled, setIsBaseEnabled] = useState<boolean>(false);
+
+    useEffect(() => {
+        const satisfiesTriggerElem = () => {
+            switch (triggerElem) {
+                case 'base':
+                    return isBaseHovered;
+                case 'base-or-overlay':
+                    return isBaseHovered || isOverlayHovered;
+            }
+        };
+
+        const satisfiesTriggerRestraint = () => {
+            if (!triggerRestraint) {
+                return true;
+            }
+
+            switch (triggerRestraint) {
+                case 'only-enabled':
+                    return isBaseEnabled;
+            }
+        };
+
+        setShowOverlay(satisfiesTriggerElem() && satisfiesTriggerRestraint());
+    }, [
+        triggerElem,
+        triggerRestraint,
+        isBaseEnabled,
+        isBaseHovered,
+        isOverlayHovered,
+    ]);
+
+    return (
+        <div {...attrs}>
+            <OverlayContext
+                value={{
+                    setIsBaseEnabled,
+                    setIsBaseHovered,
+                    setIsOverlayHovered,
+                    showOverlay,
+                }}
+            >
+                {children}
+            </OverlayContext>
+        </div>
+    );
 };
 
 Overlay.Base = OverlayBase;
