e7bf252a8a7a23798af7e3c4eec73d78ea151abe
maeldroem <maeldroem@proton.me>
2026-04-21 18:05:18 +0200

    Fixed tests

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
+/* eslint-disable max-classes-per-file */
+
 import '@testing-library/jest-dom/extend-expect';
 
 import { configure } from '@testing-library/dom';
@@ -17,3 +19,75 @@ window.ResizeObserver = class {
     disconnect() {} // eslint-disable-line no-empty-function,class-methods-use-this
     unobserve() {} // eslint-disable-line no-empty-function,class-methods-use-this
 };
+
+// See https://aronschueler.de/blog/2022/12/14/mocking-intersectionobserver-in-jest/
+// Mock the IntersectionObserver, see https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+window.IntersectionObserver = class {
+    root = null;
+    rootMargin = '';
+    thresholds = [];
+
+    // eslint-disable-next-line class-methods-use-this
+    disconnect() {
+        return null;
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    observe() {
+        return null;
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    takeRecords() {
+        return [];
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    unobserve() {
+        return null;
+    }
+};
+
+window.DOMRect = class {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+
+    constructor(
+        x?: number | undefined,
+        y?: number | undefined,
+        width?: number | undefined,
+        height?: number | undefined,
+    ) {
+        this.x = x ?? 0;
+        this.y = y ?? 0;
+        this.width = width ?? 0;
+        this.height = height ?? 0;
+    }
+
+    get top() {
+        return this.y;
+    }
+
+    get left() {
+        return this.x;
+    }
+
+    get bottom() {
+        return this.y + this.height;
+    }
+
+    get right() {
+        return this.x + this.width;
+    }
+
+    // eslint-disable-next-line class-methods-use-this, no-empty-function
+    toJSON() {}
+
+    static fromRect(_rect?: DOMRectInit | undefined): DOMRect {
+        return new DOMRect();
+    }
+};
+
+window.Element.prototype.checkVisibility = () => true;
