7b6c628b49cc22f62b840722f0161d0ad638ec7e
SlavaNordic <265652304+SlavaNordic@users.noreply.github.com>
2026-04-22 19:12:21 +0200

    Update Card.tsx: restore card header position (#1116)
    * Update Card.tsx: restore card header position
    
    * eslint
    
    * Changed Card gap from 16px to 14px to match old style
    
    * bump up version
    
    ---------
    
    Co-authored-by: maeldroem <maeldroem@proton.me>

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -60,7 +60,7 @@ interface CardHeaderComponent extends React.FC<CardHeaderProps> {
 const CardHeader: CardHeaderComponent = ({ children, className, ...attrs }) => (
     <header
         className={classNames(
-            `tw-border-b tw-border-solid tw-border-b-black tw-border-opacity-10 tw-py-4`,
+            `tw-border-b tw-border-solid tw-border-b-black tw-border-opacity-10 tw-pb-3 tw-pt-5`,
             className,
         )}
         {...attrs}
@@ -98,7 +98,7 @@ interface CardComponent extends React.FC<CardProps> {
 const Card: CardComponent = ({ children, className, ...attrs }) => (
     <article
         className={classNames(
-            `tw-preflight tw-relative tw-flex tw-flex-col tw-gap-4 tw-break-words tw-border tw-border-solid tw-border-black tw-border-opacity-10 tw-bg-white tw-px-4 tw-pb-4`,
+            `tw-preflight tw-relative tw-flex tw-flex-col tw-gap-3.5 tw-break-words tw-border tw-border-solid tw-border-black tw-border-opacity-10 tw-bg-white tw-px-4 tw-pb-4`,
             className,
         )}
         {...attrs}
